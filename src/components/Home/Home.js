b9afa7ea9e11a65cb68153d21bd3f1e7f9936338
jazdosanjh <jas@dosanjh.co.uk>
2019-03-28 11:24:09 +0000

    Imported the TMDB API using the fetch method in our app.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { API_URL, API_KEY, IMAGE_BASE_URL, POSTER_SIZE, BACKDROP_SIZE } from '../../config';
 import HeroImage from '../elements/HeroImage/HeroImage';
 import SearchBar from '../elements/SearchBar/SearchBar';
 import FourColGrid from '../elements/FourColGrid/FourColGrid';
@@ -10,7 +11,44 @@ import './Home.css';
 
 class Home extends Component {
     state = {
+        movies: [],
+        heroImage: null,
+        loading: false,
+        currentPage: 0,
+        totalPages: 0,
+        searchTerm: ''
+    }
 
+    componentDidMount() {
+        this.setState({ loading: true });
+        const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+        this.fetchItems(endpoint);
+    }
+
+    loadMoreItems = () => {
+        let endpoint = '';
+        this.setState({ loading: true });
+
+        if (this.state.searchTerm === '') {
+            endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=${this.state.currentPage + 1}`;
+        } else {
+            endpoint = `${API_URL}search/movie?api_key=${API_KEY}&language=en-US&query${this.state.searchTerm}&page=${this.state.currentPage + 1}`;
+        }
+        this.fetchItems(endpoint);
+    }
+
+    fetchItems = (endpoint) => {
+        fetch(endpoint)
+        .then(result => result.json())
+        .then(result => {
+            this.setState({
+                movies: [...this.state.movies, ...result.results],
+                heroImage: this.state.heroImage || result.results[0],
+                loading: false,
+                currentPage: result.page,
+                totalPages: result.totalPages
+            })
+        })
     }
 
     render() {
