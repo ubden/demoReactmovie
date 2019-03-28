7ff4cc86934d188920c4c64b16dc649b73b0d8c6
jazdosanjh <jas@dosanjh.co.uk>
2019-03-28 18:06:10 +0000

    Created the Navigation, and Movie Information component for the movie page.

--- src/components/elements/Navigation/Navigation.js
+++ src/components/elements/Navigation/Navigation.js
@@ -4,8 +4,14 @@ import './Navigation.css';
 
 const Navigation = (props) => {
     return (
-        <div>
-            Navigation
+        <div className="rmdb-navigation">
+            <div className="rmdb-navigation-content">
+                <Link to="/">
+                    <p>Home</p>
+                </Link>
+                <p>/</p>
+                <p>{props.movie}</p>
+            </div>
         </div>
     )
 }
