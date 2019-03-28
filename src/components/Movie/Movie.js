0b6afbd3df26e586bb2f32bda0458ef9657bde9c
jazdosanjh <jas@dosanjh.co.uk>
2019-03-28 17:28:47 +0000

    Created initial movie component files that I will need for the movie section of the app.

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
+import { API_URL, API_KEY } from '../../config';
+import Navigation from '../elements/Navigation/Navigation';
+import MovieInfo from '../elements/MovieInfo/MovieInfo';
+import MovieInfoBar from '../elements/MovieInfoBar/MovieInfoBar';
+import FourColGrid from '../elements/FourColGrid/FourColGrid';
+import Actor from '../elements/Actor/Actor';
+import Spinner from '../elements/Spinner/Spinner';
+import './Movie.css';
 
 class Movie extends Component {
     render() {
         return (
-            <div>
-                Movie
+            <div className="rmdb-movie">
+                <Navigation />
+                <MovieInfo />
+                <MovieInfoBar />
+                <FourColGrid />
+                <Spinner />
             </div>
         )
     }
