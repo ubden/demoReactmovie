39d65756c0c446c5c5017fe8c97b4285d8a986f2
jazdosanjh <jas@dosanjh.co.uk>
2019-03-29 10:08:15 +0000

    Configured app for deployment.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,7 +7,7 @@ import NotFound from '../elements/NotFound/NotFound';
 
 const App = () => {
     return (
-        <BrowserRouter>
+        <BrowserRouter basename='/rmdb/'>
             <React.Fragment>
                 <Header />
                 <Switch>
