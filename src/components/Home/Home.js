1f29dbfb05e3e93b03701684d099ef3a0a01179d
jazdosanjh <jas@dosanjh.co.uk>
2019-03-29 09:54:32 +0000

    Set up local storage to keep the state for users. This will now allow local storage to remember which movies users have clicked on, and also which movies users search for. This will improve the performance of the app on the users machine, and it will also enhance the user experience as they will be able to return to previous search results.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -20,9 +20,14 @@ class Home extends Component {
     }
 
     componentDidMount() {
-        this.setState({ loading: true });
-        const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
-        this.fetchItems(endpoint);
+        if (localStorage.getItem('HomeState')) {
+            const state = JSON.parse(localStorage.getItem('HomeState'));
+            this.setState({ ...state });
+        } else {
+            this.setState({ loading: true });
+            const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+            this.fetchItems(endpoint);
+        }
     }
 
     searchItems = (searchTerm) => {
@@ -63,8 +68,13 @@ class Home extends Component {
                 loading: false,
                 currentPage: result.page,
                 totalPages: result.totalPages
+            }, () => {
+                if (this.state.searchTerm === "") {
+                localStorage.setItem('HomeState', JSON.stringify(this.state));
+                }
             })
         })
+        .catch(error => console.error('Error:', error))
     }
 
     render() {
--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -17,10 +17,15 @@ class Movie extends Component {
     }
 
     componentDidMount() {
+        if (localStorage.getItem(`${this.props.match.params.movieId}`)) {
+            const state = JSON.parse(localStorage.getItem(`${this.props.match.params.movieId}`));
+            this.setState({ ...state });
+        } else {
         this.setState({ loading: true })
         // First fetch the movie ...
         const endpoint = `${API_URL}movie/${this.props.match.params.movieId}?api_key=${API_KEY}&language=en-US`;
         this.fetchItems(endpoint);
+        }
     }
 
     fetchItems = (endpoint) => {
@@ -43,6 +48,8 @@ class Movie extends Component {
                             actors: result.cast,
                             directors,
                             loading: false
+                        }, () => {
+                            localStorage.setItem(`${this.props.match.params.movieId}`, JSON.stringify(this.state));
                         })
                     })
                 })
