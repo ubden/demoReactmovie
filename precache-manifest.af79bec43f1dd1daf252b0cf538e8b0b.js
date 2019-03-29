00c57e343ac2ab1b5051c196e5554cd60fc1a054
jazdosanjh <jas@dosanjh.co.uk>
2019-03-29 10:03:08 +0000

    Updates

---
+++ precache-manifest.af79bec43f1dd1daf252b0cf538e8b0b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b042062c725c809db5b3",
+    "url": "/rmdb/static/js/runtime~main.93b756b4.js"
+  },
+  {
+    "revision": "5d27890cb736de8e1df6",
+    "url": "/rmdb/static/js/main.2c8d8efc.chunk.js"
+  },
+  {
+    "revision": "fa9d3f6d70da8c7c74ff",
+    "url": "/rmdb/static/js/2.444b659d.chunk.js"
+  },
+  {
+    "revision": "5d27890cb736de8e1df6",
+    "url": "/rmdb/static/css/main.dba863b1.chunk.css"
+  },
+  {
+    "revision": "b4bdf9e86661db82b31b05108ec221b8",
+    "url": "/rmdb/index.html"
+  }
+];
